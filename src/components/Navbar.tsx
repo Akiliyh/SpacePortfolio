33a811fb0c445fca1bea11c4156df7600aed9890
Akiliyh <akiliyhsensei@gmail.com>
2025-08-23 20:28:37 +0200

    feat: added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-import React from "react"
+import logo from '../assets/GBRDrop.png';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar">
+      <div className="content">
+      <h1 className="tag">GBR</h1>
+      <div className="tabs">
+        <span>About</span>
+        <span>Contact</span>
+        <img src={logo} alt="" />
+      </div>
+      </div>
+
+    </nav>
   )
 };
 
