b4aa0197d1e579d51ba47acd7003ab4f78ef3225
Akiliyh <akiliyhsensei@gmail.com>
2025-08-24 11:43:32 +0200

    feat: added intro motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react'
 import './App.css'
 import { Intro, Navbar, Project, Canvas, Title } from './components';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap'
 
 
 
@@ -8,13 +10,29 @@ import { Intro, Navbar, Project, Canvas, Title } from './components';
 
 function App() {
 
+  const appContentRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.from(appContentRef.current, {
+      delay: 3,
+      duration: 2,
+      ease: "expo.out",
+      yPercent: 100,
+      opacity: 0,
+    })
+  })
+
+
   return (
     <>
-    <Intro></Intro>
-    <Navbar></Navbar>
-      <Canvas>
-        <Title></Title>
-      </Canvas>
+      <Intro></Intro>
+      <div className="app-content" ref={appContentRef}>
+        <Navbar></Navbar>
+        <Canvas>
+          <Title></Title>
+        </Canvas>
+      </div>
     </>
   )
 }
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,9 +1,120 @@
-import React from "react"
+import { useRef, useState, useEffect } from "react"
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap'
+import { SplitText } from "gsap/SplitText";
+import logo from '../assets/GBRDrop.png';
+
+gsap.registerPlugin(useGSAP, SplitText);
 
 const Intro = () => {
+
+  const titleRef = useRef<HTMLDivElement>(null);
+  const introContainerRef = useRef<HTMLDivElement>(null);
+  const introRef = useRef<HTMLDivElement>(null);
+  const spaceRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    const parentSplit = new SplitText(titleRef.current, {
+      type: "lines",
+      linesClass: "split-parent",
+      reduceWhiteSpace:false
+    });
+
+    const childSplit = new SplitText(titleRef.current, { type: "chars", reduceWhiteSpace:false});
+
+    let initials = childSplit.chars.filter(el =>
+      ["G", "B", "R"].includes(el.textContent)
+    );
+
+    const allOtherLetters = childSplit.chars.filter(
+      el => !["G", "B", "R"].includes(el.textContent)
+    );
+
+    const capitalR = childSplit.chars.filter(
+      el => ["R"].includes(el.textContent)
+    );
+
+    console.log(allOtherLetters);
+
+    const tl = gsap.timeline();
+
+    tl.add('start')
+    
+      .from(initials, {
+        duration: 2,
+        ease: "expo.out",
+        yPercent: 100,
+        opacity: 0,
+        stagger: 0.4,
+      }, 'start')
+      // .to(initials, {
+      //   delay: 5
+      // })
+      .to(allOtherLetters, {
+        xPercent: -100,
+        width: 0,
+        duration: 0,
+      }, 'start')
+      .to(allOtherLetters, {
+        width: "auto",
+        delay: 1.8,
+        xPercent: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'expo.out'
+      }, 'start')
+    .to(spaceRef.current, {
+        xPercent: -100,
+        width: 0,
+        duration: 0,
+      }, 'start')
+      .to(spaceRef.current, {
+        width: "auto",
+        delay: 1.8,
+        xPercent: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'expo.out'
+      }, 'start')
+      .to(capitalR, {
+        width: 0,
+        delay: 1.8,
+        xPercent: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'expo.out'
+      }, 'start')
+      .add('finish')
+      .to(introContainerRef.current, {
+        rotationX: 100,
+        transformOrigin: "50% 50% -160px",
+        opacity: 0,
+        duration: 0.8,
+        ease: "expo.inOut",
+        stagger: 0.25
+      }, 'finish')
+      .to(introRef.current, {
+        yPercent: -100,
+        duration: 0.8,
+        ease: "expo.inOut"
+      }, 'finish');
+
+    return () => {
+      parentSplit.revert();
+      childSplit.revert();
+    };
+  }, { scope: titleRef })
+
   return (
-    <div>
-      
+    <div className="intro" ref={introRef}>
+      <div ref={introContainerRef} className="intro-container">
+        <div ref={titleRef} className="intro-title">
+          Guillaume BoucherR
+          {/* <div ref={spaceRef}/> */} 
+           
+        </div>
+        <img src={logo} alt="" />
+      </div>
     </div>
   )
 };
