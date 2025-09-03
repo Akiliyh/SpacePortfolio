4075acfd9a59eb25d18e6486d62148966913f319
Akiliyh <akiliyhsensei@gmail.com>
2025-09-03 22:06:08 +0200

    added video handling project

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,9 +1,10 @@
-import { useRef, type MouseEventHandler } from "react"
+import { useEffect, useRef, useState, type MouseEventHandler } from "react"
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import logo from '../assets/GBRDrop.png';
 import { FaReact } from "react-icons/fa";
 import { SiExpo } from "react-icons/si";
+import { RxCross2 } from "react-icons/rx";
 
 type InfoPanelProps = {
     closeProjectClick: MouseEventHandler,
@@ -14,19 +15,46 @@ type InfoPanelProps = {
 
 const InfoPanel = ({ closeProjectClick, showInfoDiv, unmountInfoDiv, projectContent }: InfoPanelProps) => {
     const infoDivRef = useRef<HTMLDivElement>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
     const backgroundFallbackRef = useRef<HTMLDivElement>(null);
     const containerRef = useRef<HTMLDivElement>(null);
 
-    console.log(projectContent);
+    const [isVideoPlaying, setIsVideoPlaying] = useState(true);
+
+    useEffect(() => {
+        if (isVideoPlaying) {
+            videoRef.current?.play();
+        } else {
+            videoRef.current?.pause();
+        }
+
+    }, [isVideoPlaying])
+
+    useEffect(() => {
+        if (isVideoPlaying) {
+            videoRef.current?.play();
+        } else {
+            videoRef.current?.pause();
+        }
+
+    }, [unmountInfoDiv])
+
+    const handleVideoClick = () => {
+        if (isVideoPlaying) {
+            setIsVideoPlaying(false);
+        } else {
+            setIsVideoPlaying(true);
+        }
+    };
 
     useGSAP(() => {
 
         if (showInfoDiv) {
-            gsap.to(infoDivRef.current, { height: window.innerHeight - 20, y: 0, duration: 1.5, ease: "expo.out" });
+            gsap.to(infoDivRef.current, { height: window.innerHeight - 20, paddingTop: 10, paddingBottom: 10, y: 0, duration: 1.5, ease: "expo.out" });
             gsap.set(backgroundFallbackRef.current, { height: window.innerHeight });
             gsap.to(backgroundFallbackRef.current, { autoAlpha: .2, y: 0, duration: 1.5, ease: "expo.out" });
         } else {
-            gsap.to(infoDivRef.current, { height: 0, duration: 1.5, ease: "expo.out", onComplete: () => { unmountInfoDiv } });
+            gsap.to(infoDivRef.current, { height: 0, paddingTop: 0, paddingBottom: 0, duration: 1.5, ease: "expo.out", onComplete: () => { unmountInfoDiv } });
             gsap.to(backgroundFallbackRef.current, { autoAlpha: 0, duration: .5, ease: "expo.out" });
             gsap.set(backgroundFallbackRef.current, { height: 0, autoAlpha: 0, delay: .1 });
         }
@@ -43,12 +71,13 @@ const InfoPanel = ({ closeProjectClick, showInfoDiv, unmountInfoDiv, projectCont
                         <h1 className="tag">GBR</h1>
                         <img src={logo} alt="" />
                     </div>
-                    <div className="cross"></div>
+                    <div className="cross" onClick={closeProjectClick}>
+                        <RxCross2 size={25} />
+                    </div>
                 </div>
 
                 <div className="content">
-                    {/* TODO ONCLICK PAUSE VIDEO */}
-                    <video key={projectContent.video} className="video" autoPlay muted loop disablePictureInPicture>
+                    <video onClick={handleVideoClick} ref={videoRef} key={projectContent.video} className="video" autoPlay muted loop disablePictureInPicture>
                         <source src={"video" + projectContent.video} type="video/mp4" />
                     </video>
                     <div className="row">
@@ -62,8 +91,8 @@ const InfoPanel = ({ closeProjectClick, showInfoDiv, unmountInfoDiv, projectCont
                     </div>
                     <div className="row">
                         <div className="icons">
-                            <FaReact />
-                            <SiExpo />
+                            <FaReact size={30}/>
+                            <SiExpo size={30}/>
                         </div>
                     </div>
                 </div>
